5ca8e1fb5c3260adc07a97d11eb074b68af846d1
sathiyan <uwusathiyancst@gmail.com>
2019-03-13 22:37:53 +0530

    first Changes Initial commit

---
+++ DentistDevAPP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DentistDevAPP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DentistDevAPP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DentistDevAPP.Startup))]
+namespace DentistDevAPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
